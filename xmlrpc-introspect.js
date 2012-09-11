f199d191e5dab13fe531095c196ca0681649c5a8
Jared Hanson <jaredhanson@gmail.com>
2012-09-10 22:13:52 -0700

    Return responses to introspect methods.

--- xmlrpc-introspect.js
+++ xmlrpc-introspect.js
@@ -1,19 +1,36 @@
 define(['exports', 'module'],
 function(exports, module) {
 
-  exports = module.exports = function(caps) {
-  
+  exports = module.exports = function(methods, options) {
+    options = options || {};
+    
     var service = new Object();
     service.listMethods = function(result) {
-      return result(null, null);
+      var m = [];
+      for (var name in methods) {
+        m.push(name);
+      }
+      return result(null, m);
     }
     
-    service.methodSignature = function(result) {
-      return result(null, null);
+    service.methodSignature = function(name, result) {
+      var method = methods[name];
+      if (!method) return result(new Error('requested method not found'))
+      
+      var s = 'undef';
+      if (method.signature) {
+        s = [ method.signature ];
+      } else if (method.signatures) {
+        s = method.signatures;
+      }
+      return result(null, s);
     }
     
-    service.methodHelp = function(result) {
-      return result(null, null);
+    service.methodHelp = function(name, result) {
+      var method = methods[name];
+      if (!method) return result(new Error('requested method not found'))
+      
+      return result(null, method.help || options.help || ('No help for ' + name));
     }
     
     return service;
